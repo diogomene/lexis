cc1f8c73c785ddae25af9eda6d168c71987a2b7e
Murilo Fernandes <murilofsilva11@gmail.com>
2024-11-23 13:27:37 -0400

    feat: adiciona lógica de baixar os livros de filosofia e ficção no formato txt

--- src/downloader/catalog/downloader_gutenberg.py
+++ src/downloader/catalog/downloader_gutenberg.py
@@ -1,6 +1,7 @@
 import requests
 import os
 import sys
+import json
 
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 from util.http_requester import getFromUrl
@@ -14,7 +15,7 @@ def get_catalog(mime_type="text", languages=["en"]):
     catalog = []
     book_count = 0
 
-    while url and book_count < 2000:
+    while url and book_count < 20:
         try:
             res = getFromUrl(url)
 
@@ -30,9 +31,44 @@ def get_catalog(mime_type="text", languages=["en"]):
 
     return catalog
 
+def get_books(catalog):
+    current_dir = os.path.dirname(os.path.abspath(__file__))
+    output_path = os.path.abspath(os.path.join(current_dir, "../../../data/catalog/books_gutenberg.json"))
+
+    new_books = []
+    next_id = 1
+
+    for book in catalog:
+        formats = book.get("formats", {})
+        text_plain_link = formats.get("text/plain; charset=us-ascii")
+
+        if not text_plain_link:
+            continue
+
+        try:
+            response = requests.get(text_plain_link)
+            response.raise_for_status()
+            content = response.text
+
+            new_book = {
+                "id": next_id,
+                "content": content
+            }
+            new_books.append(new_book)
+            next_id += 1
+
+        except requests.RequestException as e:
+            print(f"Erro ao acessar o link {text_plain_link}: {e}")
+            continue
+
+    os.makedirs(os.path.dirname(output_path), exist_ok=True)
+    with open(output_path, "w", encoding="utf-8") as file:
+        json.dump(new_books, file, indent=4, ensure_ascii=False)
+
 def download_catalog():
     catalog = get_catalog()
     current_dir = os.path.dirname(os.path.abspath(__file__))
     catalog_path = os.path.join(current_dir, "../../../data/catalog/gutenberg_catalog.json")
     catalog_path = os.path.abspath(catalog_path)
     save_json_file(catalog, catalog_path)
+    get_books(catalog)
